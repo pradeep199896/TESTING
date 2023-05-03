cf7fbf3f5d4d893d7070e872940ce6e666bc2cfb
faddywang <faddy_wang@sina.com>
2023-05-04 07:47:43 +0800

    license

--- src/main/java/com/solace/connectors/database/SolaceDBConnectorConnector.java
+++ src/main/java/com/solace/connectors/database/SolaceDBConnectorConnector.java
@@ -1,5 +1,20 @@
 package com.solace.connectors.database;
 
+/*
+ * Licensed to Solace Corporation under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * Solace licenses this file to You under the Solace Community License, Version 1.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. A copy of the License must be provided to you with the source code
+ * distribution.
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import com.solace.connectors.database.mapper.SolaceToSolaceDBConnectorPayloadMapper;
 import com.solace.connectors.database.mapper.SolaceDBConnectorToSolacePayloadMapper;
 import com.solace.connector.core.function.MapPayloadsFunctions;
